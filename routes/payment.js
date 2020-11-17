17de0ee161e4a585e0c90ce7fbe36cabe8eab536
Malo Lebrin <malolebrin@gmail.com>
2020-11-17 17:25:07 +0100

    add amount value

--- routes/payment.js
+++ routes/payment.js
@@ -9,16 +9,14 @@ require("dotenv").config;
 
 const stripe = createStripe(process.env.STRIPE_API_SECRET);
 
-// on réceptionne le token
 router.post("/payment", isAuthenticated, async (req, res) => {
     try {
-        //alller chercher le prix du produit en base de donnée en utilisant l'id du produit
         const { token, title, id, amount } = req.fields
         const offer = await Offer.findById(id)
         console.log("avant if");
         if (offer) {
             const response = await stripe.charges.create({
-                amount: offer.product_price * 100,
+                amount: (offer.product_price || amount) * 100,
                 currency: "eur",
                 description: `Paiement vinted pour : ${title}`,
                 source: token,
@@ -26,21 +24,21 @@ router.post("/payment", isAuthenticated, async (req, res) => {
             console.log("aaprès stripe avant nouvelle commande");
             const newOrder = await new Order({
                 date: new Date(),
-                amount: offer.product_price,
+                amount,
                 delivery: false,
                 products: id,
                 user: req.user._id,
             })
             console.log("après nouvelle commande avant save");
-            await newOrder.save()
+            await newOrder.save(); // c'est ici que ça merde 
             console.log('après save new order');
-            const user = await User.findById(req.user._id)
+            const user = await User.findById(req.user._id);
             console.log('après find user');
-            user.orders.push(newOrder._id)
+            user.orders.push(newOrder._id);
             console.log("après push new order to user");
-            await user.save()
+            await user.save();
             console.log("après user save");
-            await Offer.findByIdAndDelete(id)
+            await Offer.findByIdAndDelete(id);
             console.log("après delete offer");
             return res.json({
                 response: response,
